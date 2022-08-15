dd6b208be225447d5e9e96541945385a1bd363ce
KeveenMenezes <55338949+KeveenMenezes@users.noreply.github.com>
2022-08-15 16:09:26 -0300

    ajax
    aula

---
+++ ajax/ajax/server.js
@@ -0,0 +1,13 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+app.get('/teste',(req,res) => res.send('Ok'))
+app.listen(8081, () => console.log('Executando...'))
